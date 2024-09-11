a0c44171091340cae7c0e8838302cd9dc0c0d3b5
Andrew Marfo <andrewmarfo23@gmail.com>
2024-09-11 14:47:44 +0000

    onboarding screen

--- lib/pages/onboarding_page.dart
+++ lib/pages/onboarding_page.dart
@@ -6,7 +6,54 @@ class OnboardingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Hello world'),
+      backgroundColor: const Color(0xff2c2b34),
+      body: Column(
+        children: [
+          SizedBox(
+            width: double.infinity,
+            child: Image.asset(
+              'assets/onboarding.png',
+              fit: BoxFit.cover,
+            ),
+          ),
+          const Text(
+            'Premium cars. \nEnjoy the Luxury',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 32,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 10),
+          const Text(
+            'Premium and prestige car daily rental. \nExperience the thrill at a lower price.',
+            style: TextStyle(
+              color: Colors.grey,
+              fontSize: 16,
+            ),
+          ),
+          const SizedBox(height: 20),
+          SizedBox(
+            width: double.infinity,
+            height: 60,
+            child: Container(
+              margin: const EdgeInsets.symmetric(horizontal: 50),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.black,
+                ),
+                onPressed: () {},
+                child: const Text(
+                  'Rent Now!',
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
