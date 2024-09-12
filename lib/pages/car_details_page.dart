7658db0282585bf6e42f85b5e63d3226566f68b2
Andrew Marfo <andrewmarfo23@gmail.com>
2024-09-12 14:03:40 +0000

    Car deatails page

---
+++ lib/pages/car_details_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CarDetailsPage extends StatelessWidget {
+  const CarDetailsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Car details page'),
+      ),
+    );
+  }
+}
--- lib/widgets/car_card.dart
+++ lib/widgets/car_card.dart
@@ -1,4 +1,5 @@
 import 'package:car_rental_app/models/car.dart';
+import 'package:car_rental_app/pages/car_details_page.dart';
 import 'package:flutter/material.dart';
 
 class CarCard extends StatelessWidget {
@@ -10,62 +11,74 @@ class CarCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-      padding: const EdgeInsets.all(20),
-      decoration: BoxDecoration(
-          color: const Color(0xffF3F3F3),
-          borderRadius: BorderRadius.circular(20),
-          boxShadow: const [
-            BoxShadow(
-              color: Colors.black12,
-              blurRadius: 10,
-              spreadRadius: 5,
-            )
-          ]),
-      child: Column(
-        children: [
-          Image.asset('assets/car_image.png'),
-          Text(
-            car.model,
-            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return const CarDetailsPage();
+            },
           ),
-          const SizedBox(height: 20),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
-                children: [
-                  Row(
-                    children: [
-                      Image.asset('assets/gps.png'),
-                      const SizedBox(width: 5),
-                      Text(
-                        '${car.distance.toStringAsFixed(0)}.km',
-                        style: const TextStyle(fontSize: 16),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(width: 15),
-                  Row(
-                    children: [
-                      Image.asset('assets/pump.png'),
-                      const SizedBox(width: 5),
-                      Text(
-                        '${car.fuelCapacity.toStringAsFixed(0)}L',
-                        style: const TextStyle(fontSize: 16),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              Text(
-                '${car.pricePerHour}/hr',
-                style: const TextStyle(fontSize: 16),
+        );
+      },
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+        padding: const EdgeInsets.all(20),
+        decoration: BoxDecoration(
+            color: const Color(0xffF3F3F3),
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.black12,
+                blurRadius: 10,
+                spreadRadius: 5,
               )
-            ],
-          )
-        ],
+            ]),
+        child: Column(
+          children: [
+            Image.asset('assets/car_image.png'),
+            Text(
+              car.model,
+              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Row(
+                      children: [
+                        Image.asset('assets/gps.png'),
+                        const SizedBox(width: 5),
+                        Text(
+                          '${car.distance.toStringAsFixed(0)}.km',
+                          style: const TextStyle(fontSize: 16),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(width: 15),
+                    Row(
+                      children: [
+                        Image.asset('assets/pump.png'),
+                        const SizedBox(width: 5),
+                        Text(
+                          '${car.fuelCapacity.toStringAsFixed(0)}L',
+                          style: const TextStyle(fontSize: 16),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Text(
+                  '${car.pricePerHour}/hr',
+                  style: const TextStyle(fontSize: 16),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
